b7248846944eef44905af04cfc83cb8c8647f7bd
Moa Ha <moahaa03@gmail.com>
2024-04-24 18:48:17 +1200

    save images for navBar

--- client/components/Manager/ManagerNavBar.tsx
+++ client/components/Manager/ManagerNavBar.tsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
-import logo from '/images/logo.svg'
-import bricksLogo from '/images/bricks.svg'
-import plus from '/images/plus.svg'
-import notify from '/images/notify.svg'
-import complete from '/images/completeJ.svg'
-import profile from '/images/profile.svg'
+import logo from '../../styles/images/logo.svg'
+import bricksLogo from '../../styles/images/bricks.svg'
+import plus from '../../styles/images/plus.svg'
+import notify from '../../styles/images/notify.svg'
+import complete from '../../styles/images/complete.svg'
+import profile from '../../styles/images/profile.svg'
 
 function ManagerNavbar() {
   return (
@@ -15,21 +15,21 @@ function ManagerNavbar() {
             <img className="crane-logo" alt="logo" src={logo}></img>
             <img className="bricks-logo" alt="logo" src={bricksLogo}></img>
           </Link>
-          <Link className="h1-nav-link" to={`/jobs/manager`}>
+          <Link className="h1-nav-link" to={`/manager`}>
             <h1 className="h1-nav">Tradie Tracker</h1>
           </Link>
         </div>
         <div className="icon-container">
-          <Link to={`/manager/add`}>
+          <Link to={`/manager/create`}>
             <img className="nav-icon" alt="plus-icon" src={plus}></img>
           </Link>
           <Link to={`/notifications`}>
             <img className="nav-icon" alt="notify-icon" src={notify}></img>
           </Link>
-          <Link to={`/jobs/manager/complete`}>
+          <Link to={`/manager/complete`}>
             <img className="nav-icon" alt="complete-icon" src={complete}></img>
           </Link>
-          <Link to={`/jobs/manager/:id/profile`}>
+          <Link to={`/manager/:id/profile`}>
             <img className="nav-icon" alt="profile-icon" src={profile}></img>
           </Link>
         </div>
