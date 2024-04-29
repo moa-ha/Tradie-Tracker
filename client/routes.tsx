a8cc42485fdb38c9881c16a34de4ac56b74ebb69
Moa Ha <moahaa03@gmail.com>
2024-04-29 23:27:28 +1200

    make Home page

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,9 +1,9 @@
 import { createRoutesFromElements, Route } from 'react-router-dom'
-import Home from './components/Home.tsx'
 import Dashboard from './pages/Dashboard.tsx'
 import JobDetail from './pages/JobDetail.tsx'
 import CreateJob from './pages/CreateJob.tsx'
 import Completed from './pages/Completed.tsx'
+import Home from './pages/Home.tsx'
 export default createRoutesFromElements(
   <Route>
     <Route index element={<Home />} />
