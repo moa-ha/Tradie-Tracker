891196c5e99845bbe8544b6a7efd34f286abcf23
Moa Ha <moahaa03@gmail.com>
2024-04-30 13:44:27 +1200

    make employee jobcard backend route

---
+++ server/routes/employee.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+import * as db from '../db/jobs.ts'
+
+const router = Router()
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const job = await db.getJobsById(Number(id))
+
+    res.json(job)
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+export default router
