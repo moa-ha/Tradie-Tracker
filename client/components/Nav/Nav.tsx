35dd6efa11e89dedd1eafd80556ab4b16edf950c
Moa Ha <moahaa03@gmail.com>
2024-06-17 20:25:32 +1200

    put nav bar on submitform comp

--- client/components/Nav/Nav.tsx
+++ client/components/Nav/Nav.tsx
@@ -4,7 +4,7 @@ import ManagerNavbar from './ManagerNavBar'
 function Nav() {
   const user = window.location.pathname
 
-  if (user.startsWith('/employee')) {
+  if (user.startsWith('/employee') || user.startsWith('/jobs')) {
     return <EmployeeNavBar />
   } else if (user.startsWith('/manager')) {
     return <ManagerNavbar />
