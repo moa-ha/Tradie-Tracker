45b2f9bf726c83f5ad2a1c758fe0f2273d60a329
Moa Ha <moahaa03@gmail.com>
2024-09-30 23:58:52 +1300

    apply updated google map api

--- client/components/MapMarker.tsx
+++ client/components/MapMarker.tsx
@@ -1,67 +1,50 @@
-import React, { useEffect } from 'react'
-
-interface Position {
-  lat: number
-  lng: number
-}
-
-interface MapMarkerProps {
-  locations: string[]
-}
-
-const MapMarker: React.FC<MapMarkerProps> = () => {
+import { useEffect } from 'react'
+const MapMarker = () => {
+  const mapId = '9e8c71bfe65128e1'
   useEffect(() => {
-    const initMap = async () => {
-      const locations: Position[] = [
-        // put the data - now hardcoded
-        { lat: -36.7660918, lng: 174.7267817 },
-        { lat: -36.7716377, lng: 174.713434 },
-        { lat: -36.8827032, lng: 174.6331554 },
-        { lat: -36.7705578, lng: 174.7275945 },
-        { lat: -36.851687, lng: 174.7832322 },
-        { lat: -36.9383626, lng: 174.652653 },
-        { lat: -36.8786769, lng: 174.8361916 },
-        { lat: -36.9096579, lng: 174.6610737 },
-        { lat: 36.9111394, lng: 174.6103638 },
-      ]
+    const loadMap = async () => {
+      // Google Maps API를 동적으로 로드합니다.
+      const { Map } = (await google.maps.importLibrary(
+        'maps',
+      )) as google.maps.MapsLibrary
+      const { AdvancedMarkerElement } = (await google.maps.importLibrary(
+        'marker',
+      )) as google.maps.MarkerLibrary
 
-      // Load Google Maps API
-      await loadGoogleMaps()
+      const position = { lat: -36.7660918, lng: 174.7267817 } // Auckland 위치
 
-      const map = new google.maps.Map(
-        document.getElementById('map') as HTMLElement,
-        {
-          zoom: 11,
-          center: locations[0],
-        },
-      )
+      const map = new Map(document.getElementById('map') as HTMLElement, {
+        zoom: 11,
+        center: position,
+        mapId: mapId,
+      })
 
-      locations.forEach((location, index) => {
-        new google.maps.Marker({
-          position: location,
-          map: map,
-          title: `Marker ${index + 1}`,
-        })
+      new AdvancedMarkerElement({
+        map: map,
+        position: position,
+        title: 'Auckland',
       })
     }
 
-    initMap()
-  }, [])
-
-  const key = import.meta.env.VITE_MAP_API_KEY
-  const loadGoogleMaps = () => {
-    return new Promise<void>((resolve, reject) => {
+    // Google Maps API가 이미 로드되었는지 확인합니다.
+    if (window.google && window.google.maps) {
+      loadMap()
+    } else {
       const script = document.createElement('script')
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&libraries=geometry&language=en`
+      script.src = `https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap`
       script.async = true
       script.defer = true
-      script.onload = () => resolve()
-      script.onerror = (error) => reject(error)
       document.head.appendChild(script)
-    })
-  }
 
-  return <div id="map" style={{ width: '500px', height: '500px' }}></div>
+      window.initMap = loadMap // 콜백 함수로 loadMap을 설정합니다.
+    }
+  }, [])
+
+  return (
+    <div>
+      <div id="map" style={{ height: '500px', width: '500px' }}></div>
+    </div>
+  )
 }
 
 export default MapMarker
